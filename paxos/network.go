2aba8793e7ee853a1a20a5822fa1e8bfe07e668a
ciscoxll <ciscocciexl@gmail.com>
2020-06-13 01:11:48 +0800

    Implement paxos network

--- paxos/network.go
+++ paxos/network.go
@@ -1,8 +1,67 @@
 package paxos
 
-import "time"
+import (
+	"log"
+	"time"
+)
 
 type network interface {
 	send(m message)
 	recv(timeout time.Duration) (message, bool)
+}
+
+type paxosNetwork struct {
+	recvQueues map[int]chan message
+}
+
+func newPaxosNetwork(agents ...int) *paxosNetwork {
+	pn := &paxosNetwork{
+		recvQueues: make(map[int]chan message, 0),
+	}
+
+	for _, a := range agents {
+		pn.recvQueues[a] = make(chan message, 1024)
+	}
+	return pn
+}
+
+type agentNetwork struct {
+	id int
+	*paxosNetwork
+}
+
+func (pn *paxosNetwork) agentNetwork(id int) *agentNetwork {
+	return &agentNetwork{id: id, paxosNetwork: pn}
+}
+
+func (pn *paxosNetwork) send(m message) {
+	log.Printf("nt : send %+v", m)
+	pn.recvQueues[m.to] <- m
+}
+
+func (pn *paxosNetwork) empty() bool {
+	var n int
+	for i, q := range pn.recvQueues {
+		log.Printf("network %d left %d", i, len(q))
+		n += len(q)
+	}
+	return n == 0
+}
+
+func (pn *paxosNetwork) recvFrom(from int, timeout time.Duration) (message, bool) {
+	select {
+	case m := <-pn.recvQueues[from]:
+		log.Printf("network recv %+v", m)
+		return m, true
+	case <-time.After(timeout):
+		return message{}, false
+	}
+}
+
+func (an *agentNetwork) send(m message) {
+	an.paxosNetwork.send(m)
+}
+
+func (an *agentNetwork) recv(timeout time.Duration) (message, bool) {
+	return an.recvFrom(an.id, timeout)
 }
